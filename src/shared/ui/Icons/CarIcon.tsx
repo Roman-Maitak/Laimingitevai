c556ecdc641a5a4834d4aeedec34e185688e4592
Roman-Maitak <laimingitevai@outlook.com>
2024-06-21 14:43:16 +0300

    minor bug fix

--- src/shared/ui/Icons/CarIcon.tsx
+++ src/shared/ui/Icons/CarIcon.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const CarIcon = ({ color = "black" }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
--- src/shared/ui/Icons/GoodsIcon.tsx
+++ src/shared/ui/Icons/GoodsIcon.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const GoodsIcon = ({ color = "black" }) => (
   <svg
     fill={color}
